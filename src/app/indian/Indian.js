41fc11ac46f3800ee9187ce0f376c9dfe07278b7
bloor-je <james.bloor@hotmail.com>
2021-05-26 11:13:37 +0100

    hydrabadhi ingredients added

--- src/app/indian/Indian.js
+++ src/app/indian/Indian.js
@@ -13,15 +13,53 @@ class Indian extends React.Component {
         super(props);
 
         this.title = "Indian Cuisine";
-        this.recipes = ["Hydrabadhi", "Tikka Balti", "Gosht"];
+        let hydrabadhi = {
+            "name": "Hydrabadhi",
+            "brief": "A delicious creamy curry from the hydrabadhi region, made with a selection of ground spices and aromatics",
+            "cookingTime": "2h",
+            "defaultServingSize": 2,
+            "accompniment": "Chapattis or Garlic and Coriander Naan",
+            "ingredients": [{ "name": "Double Cream", "amount": "200ml", "swaps": [{ "Creme Fraische": "200ml" }, { "Milk": "100ml" }], "id": "cream" },
+                { "name": "Natural Yoghurt", "amount": "100ml", "swaps": [], "id": "yog" },
+                { "name": "Brown Onion", "amount": "1", "swaps": [{ "Shalotts": "4" }, { "Red Onion": "1" }], "id": "onion" },
+                { "name": "Tomatos", "amount": "4", "swaps": [{ "Tinned Tomatos": "400ml" }], "id": "tomatos" },
+                { "name": "Fresh Garlic", "amount": "6 Cloves", "swaps": [{ "Garlic Paste": "1 tblsp" }, {"Garlic Powder": "1/2 tblsp"}], "id": "garlic" },
+                { "name": "Fresh Ginger", "amount": "30g", "swaps": [{ "Ginger Paste": "2 tsp" }, { "Ginger Powder": "1 tsp" }], "id": "ginger" },
+                { "name": "Fresh Coriander", "amount": "30g", "swaps": [], "id": "coriander" },
+                { "name": "Fresh Chillies", "amount": "1", "swaps": [], "id": "chillies" },
+                { "name": "Olive Oil", "amount": "4 tblsp", "swaps": [{ "Vegetable Oil": "2 tblsp" }, { "Sesemee Oil": "1 tblsp" },{ "Sunflower Oil": "2 tblsp" }], "id": "oil" },
+                { "name": "Lamb", "amount": "400g", "swaps": [{ "Chicken": "400g" }, { "Beef": "300g" }], "id": "meat" },
+                { "name": "Coriander Seeds/Powder", "amount": "1 tblsp", "swaps": [], "id": "coriander" },
+                { "name": "Cumin Seeds/Powder", "amount": "1/2 tblsp", "swaps": [], "id": "cumin" },
+                { "name": "Black Cumin Seeds/Powder", "amount": "1/2 tblsp", "swaps": [], "id": "blackCumin" },
+                { "name": "Cloves", "amount": "6", "swaps": [{ "star anise": "1" }, {"fennel seeds": "1/2 tsp"}], "id": "cloves" },
+                { "name": "Black Peppercorns", "amount": "6", "swaps": [], "id": "pepper" },
+                { "name": "Poppy Seeds", "amount": "1 tblsp", "swaps": [], "id": "poppy" },
+                { "name": "Black Cardamoms (seeds only)", "amount": "2", "swaps": [{"Green Cardamoms": "4"}], "id": "cardamoms" },
+                { "name": "Dried Fenugreek Leaves", "amount": "1 tblsp", "swaps": [{ "Fenugreek Seeds": "1 tblsp" }], "id": "fenugreek" },
+                { "name": "Cinamon Powder", "amount": "1 tsp", "swaps": [{ "Cassia Bark": "10g" }, {"sugar": "1/2 tsp"}], "id": "cinamon" },
+                { "name": "Salt", "amount": "1/2 tblsp", "swaps": [], "id": "salt" },
+                { "name": "Turmeric Powder", "amount": "1/4 tblsp", "swaps": [], "id": "turnmeric" },
+                { "name": "Chillie Powder", "amount": "1/4 tblsp", "swaps": [{ "Chillie Flakes": "1/4 tblsp" }], "id": "chilliePowder" },
+                { "name": "Garam Masala", "amount": "1/2 tblsp", "swaps": [], "id": "garamMasala" },
+            ],
+        };
+
+        let tikkaBalti = { "name": "Tikka Balti" };
+        let saag = { "name": "Saag" };
+
+        this.recipes = [hydrabadhi, tikkaBalti, saag];
         this.numPages = 4;
 
         this.nextPageButton = <div className="next-page-button"><button onClick={() => this.nextPage()}>N</button></div>;
         this.prevPageButton = <div className="prev-page-button"><button onClick={() => this.prevPage()}>P</button></div>;
 
-        this.innerPage1 = <div className="page page-one">
-            <span>Choose Base Recipe</span>
-        </div>;
+        this.innerPage1 = {
+            "title": "Choose Base Recipe",
+            "type": "selection",
+            "options": this.recipes,
+            "selector": "name"
+        };
 
         this.innerPage2 = <div className="page page-two">
             <span>Personalise</span>
